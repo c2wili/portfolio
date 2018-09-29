f8f70dd2f3f870e5605bbfef238605a985ef78a7
b148399 <christopher.williams2@BNSF.com>
2018-09-29 14:20:55 -0500

    retirement pie

--- public/javascripts/summaryCharts.js
+++ public/javascripts/summaryCharts.js
@@ -188,5 +188,61 @@ function buildSummaryCharts( portfolios ){
 	        name: yr,
 	        data: [ portfolios[2].getDividend( moment() )  ] 
 	    }]
+	});
+	
+	Highcharts.chart('retirement-portfolio-chart', {
+		 chart: {
+           type:'pie',
+           height:90,
+           width:100,
+           backgroundColor: 'transparent'
+		 },
+      title: {
+          text: ''
+      },
+      subtitle: {
+          text: ''
+      },
+      legend: {
+      	borderWidth: 0
+      },
+      credits:{
+      	enabled: false
+      },
+      tooltip: {
+          formatter: function () {
+       	   console.log(this)
+              return '<b>' + this.key + '<br/> $' + $.number(this.y,2) + '</b>';
+          }
+      },
+      plotOptions: {
+          pie: {
+       	   size: 80,
+              allowPointSelect: true,
+              cursor: 'pointer',
+              dataLabels: {
+                  enabled: false,
+                  format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                  style: {
+                      color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                  }
+              }
+          }
+      },
+      series: [{
+          name: 'Accounts',
+          colorByPoint: true,
+          data: [ {
+              name: '401k',
+              y: portfolios[2].getValue(2)
+          }, {
+              name: 'Chris Roth',
+              y:  portfolios[2].getValue(3)
+          }, {
+              name: 'Dao 403b',
+              y:  portfolios[2].getValue(4)
+          }]
+      }]
 	});	
+	
 }
